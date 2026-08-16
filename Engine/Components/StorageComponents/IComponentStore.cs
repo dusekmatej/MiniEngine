c43ab3c5d80e3b6b6586396db7659cd22968425c
dusekmatej <mates.dusek@protonmail.com>
2026-08-16 19:49:13 +0200

    Removed components from main, caused errors that need to be fixed by someone

--- Engine/Components/StorageComponents/IComponentStore.cs
+++
@@ -1,6 +0,0 @@
-internal interface IComponentStore
-{
-    int Count { get; }
-    bool Has(int EntityIndex);
-    bool Remove(int EntityIndex);
-} 
--- Engine/Components/VelocityComponent.cs
+++
@@ -1,5 +0,0 @@
-
-public struct VelocityComponent : IComponent
-{
-        public Vector3 Velocity;
-}
